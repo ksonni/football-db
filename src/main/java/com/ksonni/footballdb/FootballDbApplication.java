bb4f9199033c0589a168c7107c099724bbc73087
Keshav Sonni <keshav@macbook-pro.home>
2021-09-28 19:43:20 +0100

    initial starter project

---
+++ src/main/java/com/ksonni/footballdb/FootballDbApplication.java
@@ -0,0 +1,13 @@
+package com.ksonni.footballdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksonni/footballdb/FootballDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksonni.footballdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
