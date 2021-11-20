5619f37c45844edd28da5c5b45dda24373ef26ee
Keshav Sonni <keshav.vm97@gmail.com>
2021-11-20 21:04:05 +0000

    UI: initial vue project setup

---
+++ ui/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Football DB</div>
+</template>
+
+<script lang="ts">
+
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {},
+})
+
+export default class App extends Vue {}
+
+</script>
+
+<style lang="scss">
+
+</style>
